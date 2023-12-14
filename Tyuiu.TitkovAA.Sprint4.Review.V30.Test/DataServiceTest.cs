38d82a972c92883822974aaf59432bb3b312646c
Антон <vip.tank2005@outlook.com>
2023-12-14 09:22:44 +0500

    Добавьте файлы проекта.

---
+++ Tyuiu.TitkovAA.Sprint4.Review.V30.Test/DataServiceTest.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using Tyuiu.TitkovAA.Sprint4.Review.V30.Lib;
+namespace Tyuiu.TitkovAA.Sprint4.Review.V30.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            DataService dt = new DataService();
+            int rows = 3;
+            int colums = 5;
+            int[,] mtrx = new int[rows, colums];
+            string sstr = "684259137159648";
+
+
+            Assert.AreEqual(73728, dt.Calculate(rows, colums, sstr));
+        }
+    }
+}
---
+++ Tyuiu.TitkovAA.Sprint4.Review.V30/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tyuiu.TitkovAA.Sprint4.Review.V30.Lib;
+
+namespace Tyuiu.TitkovAA.Sprint4.Review.V30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Console.WriteLine("*                                                                         *");
+            //Console.WriteLine("***************************************************************************");
+            //75симв->
+
+            Console.Title = "Спринт #4 | Выполнил: Титков А. А. | РПСб-23-1";
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: решение задания                                                   *");
+            Console.WriteLine("* Задание #7                                                              *");
+            Console.WriteLine("* Вариант #30                                                             *");
+            Console.WriteLine("* Выполнил: Титков А. А. | РПСб-23-1                                      *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Дана строка из одноразрядных цифр \"684259137159648\". Преобразуйте ее  *");
+            Console.WriteLine("* в матрицу 5 на 3 и подсчитайте произведение четных чисел.               *");
+            Console.WriteLine("***************************************************************************");
+
+            DataService dt = new DataService();
+
+            int rows = 3;
+            int colums = 5;
+            int[,] mtrx = new int[rows, colums];
+            string sstr = "684259137159648";
+
+
+            Console.WriteLine("Массив:");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write(string.Format("{0,5}", mtrx[i, j] = int.Parse(sstr.Substring(i * colums + j, 1))));
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine(dt.Calculate(rows, colums, sstr));
+
+
+        }
+    }
+}
